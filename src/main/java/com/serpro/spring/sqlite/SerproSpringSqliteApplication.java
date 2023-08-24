41b9d62885f744f46f0a4791f2bf55b384e1fca6
Alexandre Ferreira <ajferreira.br@hotmail.com>
2023-08-23 22:11:22 -0300

    initial and configs

---
+++ src/main/java/com/serpro/spring/sqlite/SerproSpringSqliteApplication.java
@@ -0,0 +1,13 @@
+package com.serpro.spring.sqlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerproSpringSqliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerproSpringSqliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serpro/spring/sqlite/SerproSpringSqliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serpro.spring.sqlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerproSpringSqliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
